c7e35a54f9e4ae867fc09477082bedeb640c8060
Sean McClure <seanmcclure@Seans-MacBook-Pro.local>
2019-11-08 15:11:10 -0500

    adding train_test_split function5

--- tetryonai/main.py
+++ tetryonai/main.py
@@ -34,7 +34,7 @@ import requests
 from bs4 import BeautifulSoup
 import cv2
 from sklearn import linear_model
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 
 DATA_PATH = pkg_resources.resource_filename('tetryonai', 'data/')
 IMG_PATH = pkg_resources.resource_filename('tetryonai', 'img/')
