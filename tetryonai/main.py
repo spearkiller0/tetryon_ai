b6f540583316c1e5d4022a98ed6b902d856b0852
Sean McClure <seanmcclure@Seans-MacBook-Pro.local>
2019-11-08 10:45:31 -0500

    adding data paths2

--- tetryonai/main.py
+++ tetryonai/main.py
@@ -1,3 +1,25 @@
+'''
+Copyright (c) 2019 Kedion Inc. (Sean McClure)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+
 import os
 import sys
 import re
@@ -32,6 +54,11 @@ def datasets(data):
             "path_to_csv" : "tetryonai/data/iris.csv"
         })
     return(res)
+
+def images(directory_name, image_name, type):
+    if(type == 'defect'):
+        # create directory is none exists
+        # copy example image  to new/existing directory
 
 def encode_and_bind(original_dataframe, feature_to_encode):
     dummies = pd.get_dummies(original_dataframe[[feature_to_encode]])
