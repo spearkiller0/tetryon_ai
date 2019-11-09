0cc902b95a29d33f8abe3008782278ed97b6f12a
Sean McClure <seanmcclure@Seans-MacBook-Pro.local>
2019-11-08 19:18:51 -0500

    adding logistic regression2

--- tetryonai/main.py
+++ tetryonai/main.py
@@ -35,7 +35,7 @@ from bs4 import BeautifulSoup
 import cv2
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
-from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, precision_score, recall_score, roc_curve, roc_auc_score
+from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, precision_score, recall_score, roc_curve, roc_auc_score, confusion_matrix
 from sklearn.linear_model import LogisticRegression
 
 DATA_PATH = pkg_resources.resource_filename('tetryonai', 'data/')
@@ -256,7 +256,7 @@ def logistic_regression(train_X, train_y, test_X, test_y):
     logreg.fit(train_X, train_y)
     model_results = {}
     predictions = logreg.predict(test_X)
-    cnf_matrix = metrics.confusion_matrix(test_y, predictions)
+    cnf_matrix = confusion_matrix(test_y, predictions)
     model_results['confusion_matrix'] = cnf_matrix
     model_results['accuracy'] = accuracy_score(test_y, predictions)
     model_results['precision'] = precision_score(test_y, predictions)
