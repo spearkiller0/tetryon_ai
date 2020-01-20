3941a62f335103b176b0b9788a68b43162db7792
Sean McClure <seanmcclure@Seans-MacBook-Pro.local>
2020-01-20 10:58:49 -0500

    fixing cnt in contours functions2

--- tetryonai/main.py
+++ tetryonai/main.py
@@ -194,26 +194,26 @@ def apply_to_list(pass_list, function):
     return(res)
 
 def extract_contours_from_image(image_path, write_path, hsv_lower, hsv_upper):
-    image = cv2.imread(image_path)
-    original = image.copy()
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    hsv_lower = np.array(hsv_lower)
-    hsv_upper = np.array(hsv_upper)
-    mask = cv2.inRange(hsv, hsv_lower, hsv_upper)
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
-    close = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel, iterations=1)
-    cnts = cv2.findContours(close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    offset = 20
-    ROI_number = 0
-    for c in cnts:
-        x, y, w, h = cv2.boundingRect(c)
-        cv2.rectangle(image, (x - offset, y - offset), (x + w + offset, y + h + offset), (36, 255, 12), 2)
-        ROI = original[y - offset:y + h + offset, x - offset:x + w + offset]
-        cv2.imwrite(write_path + 'contour_{}.png'.format(ROI_number), ROI)
-        ROI_number += 1
-    cnts = 0
+    if(os.path.getsize(image_path) > 0):
+        image = cv2.imread(image_path)
+        original = image.copy()
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        hsv_lower = np.array(hsv_lower)
+        hsv_upper = np.array(hsv_upper)
+        mask = cv2.inRange(hsv, hsv_lower, hsv_upper)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+        opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
+        close = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel, iterations=1)
+        cnts = cv2.findContours(close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+        offset = 20
+        ROI_number = 0
+        for c in cnts:
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.rectangle(image, (x - offset, y - offset), (x + w + offset, y + h + offset), (36, 255, 12), 2)
+            ROI = original[y - offset:y + h + offset, x - offset:x + w + offset]
+            cv2.imwrite(write_path + 'contour_{}.png'.format(ROI_number), ROI)
+            ROI_number += 1
 
 def write_dict_as_json(write_path, pass_dict):
     with open(write_path, 'w') as outfile:
